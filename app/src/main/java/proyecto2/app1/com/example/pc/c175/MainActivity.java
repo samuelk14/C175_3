4fcc4a1171ec24cf0d54866294afb40509b7ea82
samuelk14 <32883598+samuelk14@users.noreply.github.com>
2018-02-28 10:50:57 -0500

    Initial commit

---
+++ app/src/main/java/proyecto2/app1/com/example/pc/c175/MainActivity.java
@@ -0,0 +1,37 @@
+package proyecto2.app1.com.example.pc.c175;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button boton1, boton2, boton3, boton4, boton5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        boton1 = (Button) findViewById(R.id.boton1);
+        boton2 = (Button) findViewById(R.id.boton2);
+        boton3 = (Button) findViewById(R.id.boton3);
+        boton4 = (Button) findViewById(R.id.boton4);
+        boton5 = (Button) findViewById(R.id.boton5);
+        boton1.setOnClickListener(this);
+        boton2.setOnClickListener(this);
+        boton3.setOnClickListener(this);
+        boton4.setOnClickListener(this);
+        boton5.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        try {
+            switch (view.getId()) {
+                case R.id.boton1:
+
+            }
+        }
+
+    }
+}
